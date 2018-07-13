d2f39c84794f0374dad56d68ed0e8b1e36ee41e9
paulosalum <paulo.salum@outlook.com>
2018-07-13 17:10:22 -0300

    Shell Check fix

--- AwsSafe.sh
+++ AwsSafe.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-HIGHLIGHT="\e[01;34m"
+HIGHLIGHT='\e[01;34m'
 NORMAL='\e[00m'
 
 function update() {
 	local d="$1"
 	if [ -d "$d" ]; then
-		cd $d >/dev/null
+		cd "$d" || exit
 		if [ -d ".git" ]; then
+			rm -f "$PWD/.git/hooks/pre-commit"
 			git init
 		else
-			scan *
+			scan ./*
 		fi
-		cd .. >/dev/null
+		cd .. || exit
 	fi
 }
 
 function scan() {
-	for x in $*; do
+	for x in "$@"; do
 		update "$x"
 	done
 }
 
-git config --global init.templatedir '~/.git-templates'
+git config --global init.templatedir "$HOME/.git-templates"
 mkdir -p ~/.git-templates/hooks
 curl -o ~/.git-templates/hooks/pre-commit https://gist.githubusercontent.com/PauloSalum/47330177a5480aafd8bd76109d656fc4/raw/a25f420769a108b129e98ec4e00ed0708d8c7612/pre-commit.sh
 chmod a+x ~/.git-templates/hooks/pre-commit
 echo -e "${HIGHLIGHT}Scanning ${HOME}${NORMAL}"
-scan ${HOME}
+scan "$HOME"
